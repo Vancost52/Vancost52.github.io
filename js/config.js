5fa02ab2a0c851275d2275364bc71ce414f876e0
Vancost52 <71996429+Vancost52@users.noreply.github.com>
2020-10-03 11:29:31 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -85,7 +85,7 @@ var l_musicDisplay = true;
 /*Music playlist
 Place how much Youtube IDs/.ogg you want*/
 var l_musicPlaylist = [
-	{youtube: "aGGXkD2Al8o", name: "Relax"}
+	{youtube: "", name: "Relax"}
 ];
 
 /*Random music order?*/
